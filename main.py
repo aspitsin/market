3fe35cb8bfb7c7a87f4ead6c7704014b18a9331f
aspitsin <pz94@bk.ru>
2018-02-18 10:47:05 +0300

    add BTC and ETH func

--- main.py
+++ main.py
@@ -11,7 +11,34 @@ def get_bot_updates():
 
 result = get_bot_updates()
 
-for messages in result:
-    text = messages['message']['text']
-    chat_id = messages['message']['chat']['id']
-    print(text, chat_id)
+for message in result:
+    text = message['message']['text']
+    chat_id = message['message']['chat']['id']
+
+
+def send_mess(chat, text):
+    url = "https://api.telegram.org/bot456429804:" + token
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+#--------------------------------------------------------------------------------------------------
+
+
+def get_btc():
+    url = "https://api.cryptonator.com/api/ticker/btc-usd"
+    result = requests.get(url)
+    pydict = result.json()
+    btc = pydict['ticker']['price']
+    return btc
+
+
+def get_eth():
+    url = "https://api.cryptonator.com/api/ticker/eth-usd"
+    result = requests.get(url)
+    pydict = result.json()
+    eth = pydict['ticker']['price']
+    return eth
+
+
+print(get_btc(), get_eth())
