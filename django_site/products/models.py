ee12cae5abac1d5a1ace81f6df1134776907f575
aspitsin <34834391+aspitsin@users.noreply.github.com>
2018-02-27 09:13:16 +0300

    Stage 2 (#3)
    * add virtualenv; add Django; develop Django app
    
    * develop first page; develop product template
    
    * add three products page; add Card on products;add Navbar
    
    * add links on Category;add links on Products
    
    * add links between Categor and Products
    
    * add pagination;add ImageField

---
+++ django_site/products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Создаем базовую модель нашего продукта
+class Product(models.Model):
+      # и указываем максимальную длину
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=5000, blank=True)
+    image = models.ImageField(upload_to='media/%Y/%m/%d', blank=True)
+    category = models.ForeignKey(
+        'Category', on_delete='CASCADE', related_name='products', null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=5000, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ django_site/products/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.views import generic
+# импортируем нашу модель
+from .models import Product
+from .models import Category
+
+
+class IndexView(generic.ListView):
+    template_name = 'products_list.html'  # подключаем наш Темплейт
+    context_object_name = 'products'  # под каким именем передадутся данные в Темплейт
+    model = Product  # название Модели
+    paginate_by = 6
+    queryset = Product.objects.all()
+
+
+class ProductDetail(generic.DetailView):
+    template_name = 'product_detail.html'
+    model = Product
+
+
+class CategoryDetail(generic.DetailView):
+    template_name = 'category_detail.html'
+    model = Category
+
+
+class CategoryList(generic.ListView):
+    template_name = 'category_list.html'
+    context_object_name = 'categorys'
+    model = Category
