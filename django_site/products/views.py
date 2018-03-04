63ed5f98d9736f38a2b92f75854726f494516c29
aspitsin <pz94@bk.ru>
2018-03-04 23:04:38 +0300

    add model Order; add LoginView; add SignupView; add bootstrapform

--- django_site/products/views.py
+++ django_site/products/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 # импортируем нашу модель
 from .models import Product
 from .models import Category
+from .models import Order
 
 
 class IndexView(generic.ListView):
@@ -12,6 +15,13 @@ class IndexView(generic.ListView):
     model = Product  # название Модели
     paginate_by = 6
     queryset = Product.objects.all()
+    # метод для добавления дополнительной информации в контекст
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # передаем в словарь контекста список всех категорий
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class ProductDetail(generic.DetailView):
@@ -28,3 +38,32 @@ class CategoryList(generic.ListView):
     template_name = 'category_list.html'
     context_object_name = 'categorys'
     model = Category
+
+
+class ProductCreate(generic.CreateView):
+    model = Product
+    # название нашего шаблона с формой
+    template_name = 'product_new.html'
+    # какие поля будут в форме
+    fields = '__all__'
+
+
+class OrderFormView(generic.CreateView):
+    model = Order
+    template_name = 'order_form.html'
+    success_url = '/'
+    fields = 'customer_name', 'customer_phone'
+
+    def form_valid(self, form):
+        # получаем ID из ссылки и передаем в ORM для фильтрации
+        product = Product.objects.get(id=self.kwargs['pk'])
+        # передаем в поле товара нашей формы отфильтрованный товар
+        form.instance.product = product
+        # super — перезагружает форму, нужен для работы
+        return super().form_valid(form)
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
